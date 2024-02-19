1e721573b73b62405fdbcfc2341c3f60dab86b43
Thomas King <102960825+tkgnm@users.noreply.github.com>
2024-02-19 12:30:36 +0000

    Use cache-dependency-path, update snapshot

--- cdk/lib/my-guardian-suggested-tags.test.ts
+++ cdk/lib/my-guardian-suggested-tags.test.ts
@@ -5,7 +5,7 @@ import { MyGuardianSuggestedTags } from "./my-guardian-suggested-tags";
 describe("The MyGuardianSuggestedTags stack", () => {
   it("matches the snapshot", () => {
     const app = new App();
-    const stack = new MyGuardianSuggestedTags(app, "MyGuardianSuggestedTags", { stack: "mobile", stage: "TEST" });
+    const stack = new MyGuardianSuggestedTags(app, "MyGuardianSuggestedTags", { stack: "mobile", stage: "TEST", app: "my-guardian-suggested-tags-static" },);
     const template = Template.fromStack(stack);
     expect(template.toJSON()).toMatchSnapshot();
   });
